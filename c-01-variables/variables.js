61b7cb8c69d82f460a05a2910991290b5a940746
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-01-30 23:16:30 -0500

    Ej variables js

---
+++ c-01-variables/variables.js
@@ -0,0 +1,11 @@
+// console.log ('Hola mundo JS!')
+var nombre = 'Mario', apellido = ' Fernandez'
+var edad = 38
+
+edad = '38 años'
+
+console.log ('Hola'+' '+ nombre+''+ apellido )
+console.log ('Tengo ' + edad)
+
+
+var peso = 75
