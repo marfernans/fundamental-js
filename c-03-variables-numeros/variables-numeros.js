b22ddea4d88ef72ae1a18cbe74c7b0088d510600
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-02-01 08:40:58 -0500

    variables-numeros

---
+++ c-03-variables-numeros/variables-numeros.js
@@ -0,0 +1,47 @@
+var edad = 38
+
+// edad = edad + 2
+
+edad += 2
+
+var peso = 100
+
+//peso = peso - 30
+
+peso -= 30
+
+var sandwich = 5
+
+peso += sandwich
+
+var jugarAlFubol = 10
+
+peso -= jugarAlFubol
+
+
+var precioVino = 200.7
+
+// var total = precioVino * 3
+
+// var total = precioVino * 100 * 3 / 100
+
+var total = Math.round(precioVino * 100 * 3) / 100
+
+var totalStr = total.toFixed(3)
+
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+
+var personas = 2
+
+var catidadDePorcionesPorPersona = pizza / personas
+
+
+
+
+
+
+
+
+
