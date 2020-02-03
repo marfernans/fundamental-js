ffe1425d31045295874a8487b841c90922634eea
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-02-02 21:58:24 -0500

    Funciones

---
+++ c-03-funciones/funciones.js
@@ -0,0 +1,13 @@
+var nombre = 'Mario', edad = 39
+
+function imprimirEdad(n,e){
+    console.log(`${n} tiene ${e} años de nacido`)
+}
+
+imprimirEdad( nombre, edad)
+imprimirEdad('Miguel', 15)
+imprimirEdad('Ronald', 20)
+imprimirEdad('Angey', 10)
+imprimirEdad('marius', 25)
+imprimirEdad(50, 'Juaquito')
+
